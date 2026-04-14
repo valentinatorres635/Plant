90081d24ff3120f0533d4ad1d26c656a9220b638
Valentina Castaneda-Torres <vtorres32@gatech.edu>
2026-04-14 15:29:46 -0400

    Add plant data to plants.js

---
+++ plants.js
@@ -0,0 +1,67 @@
+[
+  {
+    "name": "Monstera Deliciosa",
+    "aliases": ["monstera", "monstera deliciosa", "swiss cheese plant"],
+    "image": "https://plus.unsplash.com/premium_photo-1665408511551-525a24a75ac5?q=80&w=687&auto=format&fit=crop"
+  },
+  {
+    "name": "Oreja de burro",
+    "aliases": ["oreja de burro", "snake plant", "sansevieria", "dracaena trifasciata", "mother in law's tongue", "lengua de suegra"],
+    "image": "https://images.unsplash.com/photo-1687552212914-03a30c82053c?q=80&w=715&auto=format&fit=crop"
+  },
+  {
+    "name": "Tulipan",
+    "aliases": ["tulipan", "tulip"],
+    "image": "https://images.unsplash.com/photo-1745315220587-9a3aded6b074?q=80&w=735&auto=format&fit=crop"
+  },
+  {
+    "name": "Sabila",
+    "aliases": ["sabila", "savila", "aloe vera", "aloe"],
+    "image": "https://images.unsplash.com/photo-1570295835271-04c05b4ed943?q=80&w=1170&auto=format&fit=crop"
+  },
+  {
+    "name": "Pothos",
+    "aliases": ["pothos", "golden pothos", "devils ivy", "devil's ivy", "epipremnum aureum"],
+    "image": "https://images.unsplash.com/photo-1596724878582-76f1a8fdc24f?q=80&w=764&auto=format&fit=crop"
+  },
+  {
+    "name": "Bromelia",
+    "aliases": ["bromelia", "bromelia azul"],
+    "image": "https://lh3.googleusercontent.com/pw/AP1GczPHALnaQ99JIJ2XlJQCzMcbW6yO7fTALmhuB3G48tRkpHPu5SiQsGVG_S9GNdIOkF8bwX7fRBZX8GVAsYR2D56GPtIZO0O9hJBm_UeshVINgiDA9MN8emecCSQ9yL7NCNZQhAMzAJrwkSaBLQZmdA4ZYQ=w624-h828-s-no-gm?authuser=0"
+  },
+  {
+    "name": "Orquidea",
+    "aliases": ["orquidea", "orquidea amarilla", "orchid"],
+    "image": "https://lh3.googleusercontent.com/pw/AP1GczO3WLgUIXNfPzKYQ7RlZWylbZGOvB9PCS6Etw59W42tdSnAXbcLtvVwIwMU3mQJn02bHvYuoYDWNt5Pf-1Uy4qLVcvL4N51kHAscrTK3P9SsNkeOZo0QPS7x_CNDBoE7ObIAkNObDHXK4qPwQfhIODNAQ=w624-h828-s-no-gm?authuser=0"
+  },
+  {
+    "name": "No me acuerdo",
+    "aliases": ["no me acuerdo"],
+    "image": "https://lh3.googleusercontent.com/pw/AP1GczPDgNUVqI7HZkX3NMU3XBGTHl1KyouFnBL2FfTZQbSJ-yiLZqHSCzrQdMYySfU7JoUMrYcnyY5gynOk3uOfH-UXV1muMdGt3wAAlUVYDpEqvWsRUbAWeGG4c3_md-_mswApAyrbxoRD7J5GVTX6feMEqA=w624-h828-s-no-gm?authuser=0"
+  },
+  {
+    "name": "Hortensia",
+    "aliases": ["hortensia", "hortensias", "hydrangea", "hydrangeas"],
+    "image": "https://images.unsplash.com/photo-1623201530307-c1d678f68459?q=80&w=735&auto=format&fit=crop"
+  },
+  {
+    "name": "Platanera",
+    "aliases": ["platanera", "bananera", "banana plant"],
+    "image": "https://images.unsplash.com/photo-1551551313-fe7031e26248?q=80&w=687&auto=format&fit=crop"
+  },
+  {
+    "name": "Helecho",
+    "aliases": ["helecho", "helechos", "fern", "ferns"],
+    "image": "https://images.unsplash.com/photo-1531156992292-d36397ee9184?q=80&w=1170&auto=format&fit=crop"
+  },
+  {
+    "name": "Cerezo",
+    "aliases": ["cerezo", "cherry tree", "sakura", "cherry blossom", "arbol de cerezo"],
+    "image": "https://images.unsplash.com/photo-1657114950534-d1120c1bd83b?q=80&w=1200&auto=format&fit=crop"
+  },
+  {
+    "name": "Azalea",
+    "aliases": ["azalea", "azaleas"],
+    "image": "https://images.unsplash.com/photo-1653006473983-b16ce21624e1?q=80&w=1170&auto=format&fit=crop"
+  }
+]
